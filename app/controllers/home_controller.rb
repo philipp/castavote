ca717c484735320d9ddb9bea6b4d718c47a72033
dmitryame <dmitryme@gmail.com>
2008-09-03 11:19:33 -0400

    added pertty charts

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,10 @@
 class HomeController < ApplicationController
   #this method is a delegator that redirects to other controllers
   def index
-    if current_user.companies.size == 1
+    if current_user and current_user.companies.size == 1
       redirect_to company_url(current_user.companies.first)
     else
-      redirect_to company_url
+      redirect_to companies_url
     end
   end
 end
