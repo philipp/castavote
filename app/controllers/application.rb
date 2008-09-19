8ec2c6f641db0438bc56ac9ae72303cb9f153763
dmitryame <dmitryme@gmail.com>
2008-09-19 09:03:37 -0400

    performance optimisation, making one request to preallowed per castavote requist

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -4,6 +4,7 @@
 class ApplicationController < ActionController::Base
   include AuthenticatedSystem  
   helper :all # include all helpers, all the time
+  before_filter :check_preallowed_for_current_request
   before_filter :has_access, :only => [:edit, :destroy, :create, :update, :new]
   
   
@@ -17,10 +18,20 @@ class ApplicationController < ActionController::Base
   # from your application log (in this case, all fields with names like "password"). 
   # filter_parameter_logging :password
   
+  private
+  
+  def check_preallowed_for_current_request
+     @company = current_user.companies.find(params[:company_id]) if !current_user.blank? && !params[:company_id].blank?
+    if(current_user and @company and !current_user.can_manage_company?(@company))      
+      session[:has_access] = true
+    else
+      session[:has_access] = false      
+    end
+  end
+  
 
   def has_access
-    @company = current_user.companies.find(params[:company_id]) if !current_user.blank? && !params[:company_id].blank?
-   if(current_user and @company and !current_user.can_manage_company?(@company))      
+   if(session[:has_access])      
      redirect_to insufficient_path
    end
   end
